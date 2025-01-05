b3dbfebdb021bf13d51cd1ff5b0ecf941f639010
Juliusz Zolich <245971@edu.p.lodz.pl>
2025-01-05 14:43:59 +0100

    Spring configuration

---
+++ src/main/java/org/example/WebApplication/ZpoProjektSerwerApplication.java
@@ -0,0 +1,13 @@
+package org.example.WebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpoProjektSerwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpoProjektSerwerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZPO_Projekt_Serwer/ZpoProjektSerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZPO_Projekt_Serwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpoProjektSerwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
