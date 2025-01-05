37317d4908716cedca5fb4d36e85ba3777b368cc
VitoBambino <witek99.pl@gmail.com>
2025-01-05 19:10:06 +0100

    poprawka nazy bazy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import org.example.WebApplication.HttpApplication;
 public class Main {
     public static void main(String[] args) {
 
-        DatabaseUtils.ConnectToDatabase("database.db"); // Baza danych
+        DatabaseUtils.ConnectToDatabase("mydb.db"); // Baza danych
         DatabaseUtils.ExecuteUpdate("CREATE TABLE IF NOT EXISTS students (indeks INTEGER PRIMARY KEY, imie TEXT, nazwisko TEXT, grupa INTEGER)");// Tabela użytkowników// Tabela ocen
         //DatabaseUtils.ExecuteUpdate("INSERT INTO students (indeks, imie, nazwisko, grupa) VALUES ('245971','Jan', 'Kowalski', 1)"); // Dodanie przykładowego użytkownika
         HttpApplication.main(new String[]{}); // Strona internetowa dla studenta
