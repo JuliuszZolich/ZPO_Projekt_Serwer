f150e3d7e0c476216913bfb6b068a64831b430a4
Juliusz Zolich <245971@edu.p.lodz.pl>
2025-01-06 15:15:20 +0100

    Cookie Update

--- src/main/java/org/example/WebApplication/ApiController.java
+++ src/main/java/org/example/WebApplication/ApiController.java
@@ -1,5 +1,6 @@
 package org.example.WebApplication;
 
+import org.example.DatabaseUtils;
 import org.example.WebApplication.Objects.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,30 +11,70 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
     @PostMapping("/api/dodajstudenta")
     public String AddStudent(@RequestBody Student student) {
-        return student.getNazwisko();
+        DatabaseUtils.ExecuteUpdate("INSERT INTO Studenci VALUES (" +
+                student.getIndeks() + ", '" +
+                student.getImie() + "', '" +
+                student.getNazwisko() + "', " +
+                student.getGrupa_id() + ", '" +
+                student.getHaslo() + "')");
+        return "";
     }
+
+    @PostMapping("/api/login")
+    public String Login() {
+        return """
+                {
+                "id": 2137,
+                "imie": "Jan",
+                "nazwisko": "Kowalski",
+                "haslo": ""
+                }
+                """;
+    }
+
     @PostMapping("/api/usunstudenta")
-    public String RemoveStudent() {
-        return ":D";
+    public String RemoveStudent(@RequestBody int id) {
+        int ret = DatabaseUtils.ExecuteUpdate("DELETE FROM Studenci WHERE id = " + id);
+        if (ret != 1) {
+            return String.format("""
+                    {
+                    "error": "Nie ma takiego studenta",
+                    "error_value": %d
+                    }
+                    """, id);
+        }
+        return """
+                {
+                "error": "",
+                "error_value": 200
+                }
+                """;
     }
+
     @PostMapping("/api/dodajstudentagrupa")
     public String AddStudentFromGroup() {
         return ":D";
     }
+
     @PostMapping("/api/usunstudentagrupa")
     public String RemoveStudentFromGroup() {
         return ":D";
     }
+
     @PostMapping("/api/dodajtermin")
     public String AddTerm() {
         return ":D";
     }
+
     @GetMapping("/api/sprawdzobecnosc")
     public String CheckAttendance() {
         return ":D";
     }
+
     @GetMapping("/api/dziennik")
     public String CheckAttendanceList() {
-        return ":D";
+        return """
+                {"id": 2137}
+                """;
     }
 }
